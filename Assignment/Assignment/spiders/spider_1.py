2fe5f4b0c81c2c3d6e14d2d3ffd4e2554589f378
printoy <printoysibi0@gmail.com>
2024-11-15 08:53:47 +0530

    next page crawling along with single data extraction

--- Assignment/Assignment/spiders/spider_1.py
+++ Assignment/Assignment/spiders/spider_1.py
@@ -15,8 +15,10 @@ class spider_1(scrapy.Spider):
         for links in link:
             absolute_url = response.urljoin(links)
             yield response.follow(absolute_url, callback=self.parse_item)
-        else:
-            self.logger.warning(f"No valid link found on {response.url}")
+        next_page = response.css('a[title="Next"]::attr(href)').get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(next_page_url, callback=self.parse)
 
     def parse_item(self, response):
         # for rooms in response.css("li.a37d52f0"):
@@ -25,10 +27,7 @@ class spider_1(scrapy.Spider):
         #         'type': rooms.xpath('.//span[@class="_19e94678 e0abc2de" and @aria-label="Type"]/text()').extract(),
         #         'links': rooms.css('a.d40f2294').attrib['href'],
         #     }
-        # next_page = response.css('a[title="Next"]::attr(href)').get()
-        # if next_page:
-        #     next_page_url = response.urljoin(next_page)
-        #     yield scrapy.Request(next_page_url, callback=self.parse)
+
         yield {
             'property_id': response.css('span._2fdf7fc5[aria-label="Reference"]::text').get(),
             'property_url': response.url,
@@ -47,7 +46,7 @@ class spider_1(scrapy.Spider):
                 'size': response.css('span._783ab618[aria-label="Area"] span::text').get()
             },
             # 'permit_number': response.css('span.e56292b8[aria-label="Permit Number"]::text').get()
-            'agent_name': response.css('a.d2d04ff3[aria-label="Agent name"] h2::text').get(),
+            'agent_name': response.css('a[aria-label="Agent name"] h2::text, span[aria-label="Agent name"]::text').get(),
             'primary_image_url':  response.css('img._4a3dac18[aria-label="Cover Photo"]::attr(src)').get(),
             'breadcrumbs':  " > ".join(response.css('a.ebd56459 span._43ad44d9::text').extract()),
             'amenities': response.css('span._7181e5ac::text').extract(),
